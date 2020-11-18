a71da76ba4ad8e78569e4a7ab7a3bb34cf599ac2
Soomyung Kang <vannskang@gmail.com>
2020-11-18 16:32:07 +0900

    first commit

---
+++ src/components/Message.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box is-info">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.box {
+  background: gray;
+  padding: 1px;
+  border: 1px solid black;
+}
+</style>
